ad0382762faf526ce95a00771353c2140aa34ce0
Nucleuz <h-hansen1990@hotmail.com>
2015-03-12 12:46:36 +0100

    Just added a class called "Player" to test if I can push and pull.

---
+++ src/example/Player.java
@@ -0,0 +1,9 @@
+package example;
+
+public class Player extends SimpleSlickGame{
+
+	public Player(String gamename) {
+		super(gamename);
+		// TODO Auto-generated constructor stub
+	}
+}
--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -24,6 +24,7 @@ public class SimpleSlickGame extends BasicGame
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
 		g.drawString("Hello World!", 250, 200);
+		g.drawRect(40,40,50,50);
 	}
 
 	public static void main(String[] args)
